c4f6924dedead7b07f9458f4d873e41f72620876
Aldo Ianelo Guerra <aldoiguerra@gmail.com>
2020-04-21 14:28:48 -0300

    gerador de numeros

--- NumerosAleatorios/ViewController.swift
+++ NumerosAleatorios/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valorSorteado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        valorSorteado.text = String(Int.random(in: 1...100));
+        
+    }
+    
 }
 
